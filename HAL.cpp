cb12e394cdc286cd6df3fe1bbe50eacf4c7bb581
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2017-10-06 18:07:49 +0200

    HAL initial commit

---
+++ HAL.cpp
@@ -0,0 +1,22 @@
+/*
+ * HAL.cpp
+ *
+ *  Created on: Oct 6, 2017
+ *      Author: cyrildz
+ */
+
+#include "HAL.h"
+
+namespace core {
+
+
+
+} /* namespace core */
+
+uint32_t core::HAL::getStateFlag(){
+	return peripheral_states;
+}
+
+void core::HAL::updatePeripheralState() {
+
+}
---
+++ HAL_test.cpp
@@ -0,0 +1,12 @@
+/*
+ * HAL_test.cpp
+ *
+ *  Created on: Oct 6, 2017
+ *      Author: cyrildz
+ */
+
+#include "HAL.h"
+
+namespace core {
+
+} /* namespace core */
